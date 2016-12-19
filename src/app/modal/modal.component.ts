57d233b275f63fed97babb04d258863802f35da7
Thomas Free <tomandhisjones@outlook.com>
2016-12-19 09:10:58 -0600

    initial commit

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,32 @@
+import {Component, ElementRef} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  host: { '(document:keyup)': 'escapePressed($event)' }
+})
+export class ModalComponent {
+  hideContainer: boolean;
+  modal: any;
+
+  constructor(modal: ElementRef) {
+    this.hideContainer = true;
+    this.modal = modal;
+  }
+
+  openModal(){
+    this.hideContainer = false;
+    this.modal.nativeElement.querySelector('.modal').focus();
+  }
+
+  closeModal(){
+    this.hideContainer = true;
+  }
+
+  escapePressed(event){
+    if(event && event.keyCode == 27){
+      this.closeModal();
+    }
+  }
+}
